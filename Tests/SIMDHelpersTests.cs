8a997c3dba42a871ea83e5c7ea8d442f813cb913
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-09-23 04:42:27 +0800

    - Added WidenTest - Added tests GH workflow

--- Tests/SIMDHelpersTests.cs
+++ Tests/SIMDHelpersTests.cs
@@ -1,15 +1,58 @@
-namespace Tests;
+using FluentAssertions;
+using Tokenizers.NET;
+using Tokenizers.NET.Collections;
 
-public class SIMDHelpersTests
+namespace Tests
 {
-    [SetUp]
-    public void Setup()
+    public sealed class SIMDHelpersTests
     {
-    }
+        private struct Config: ITokenizerConfig
+        {
+            public static uint ExpectedMaxInputLength => 1024;
+            
+            public static uint ExpectedMaxBatches => 5;
+            
+            public static string TokenizerJsonPath => "FlorenceTokenizer.json";
+        }
+        
+        private Tokenizer<Config> Tokenizer;
+        
+        [SetUp]
+        public void Setup()
+        {
+            Tokenizer = new();
+        }
 
-    [Test]
-    public void Test1()
-    {
-        Assert.Pass();
+        [TearDown]
+        public void TearDown()
+        {
+            Tokenizer.Dispose();
+        }
+        
+        [Test]
+        public void WidenTest()
+        {
+            const nuint MAX_VALUE = 500;
+            
+            for (nuint i = 1; i <= MAX_VALUE; i++)
+            {
+                using var srcBuffer = new NativeMemory<uint>(i);
+                using var destBuffer = new NativeMemory<ulong>(i);
+
+                var srcMemory = srcBuffer.Memory.AsSpan();
+                var destMemory = destBuffer.Memory.AsSpan();
+
+                var currentIndex = 0;
+                
+                foreach (ref var slot in srcMemory)
+                {
+                    slot = (uint) currentIndex++;
+                }
+
+                srcBuffer.Memory.AsReadOnly().Widen(destBuffer.Memory);
+                
+                srcMemory.ToArray().Should().BeEquivalentTo(destMemory.ToArray());
+            }
+        }
     }
 }
