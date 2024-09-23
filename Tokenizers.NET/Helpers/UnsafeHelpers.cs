6571b79062c2e145911be8553ded2883b77760be
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-09-23 12:55:58 +0800

    Added UnsafeHelpers.CalculateCastLength()

--- Tokenizers.NET/Helpers/UnsafeHelpers.cs
+++ Tokenizers.NET/Helpers/UnsafeHelpers.cs
@@ -1,7 +1,39 @@
+using System.Runtime.CompilerServices;
+
 namespace Tokenizers.NET.Helpers
 {
-    public static class UnsafeHelpers
+    public static unsafe class UnsafeHelpers
     {
-        
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static nuint CalculateCastLength<T, F>(nuint length)
+            where T: unmanaged
+            where F: unmanaged
+        {
+            // Adapted from MemoryMarshal.Cast()
+            
+            var fromSize = (nuint) sizeof(T);
+            var toSize = (nuint) sizeof(F);
+
+            var fromLength = length;
+            
+            nuint toLength;
+            
+            if (fromSize == toSize)
+            {
+                toLength = fromLength;
+            }
+            
+            else if (fromSize == 1)
+            {
+                toLength = (fromLength / toSize);
+            }
+            
+            else
+            {
+                toLength = fromLength * fromSize / toSize;
+            }
+            
+            return toLength;
+        }
     }
 }
