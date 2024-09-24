c82c17e04257c4def931988d23cffe43f56e72d5
trumpmcdonaldz <thetrumpmcdonaldz@gmail.com>
2024-09-24 17:22:40 +0800

    Added Configs.OverflowingTokenizer

--- Tests/Configs.cs
+++ Tests/Configs.cs
@@ -7,10 +7,19 @@ namespace Tests
         public struct FlorenceConfig: ITokenizerConfig
         {
             public static uint ExpectedMaxInputLength => 1024;
-            
-            public static uint ExpectedMaxBatches => 5;
+
+            public static uint ExpectedMaxBatches => 2;
             
             public static string TokenizerJsonPath => "FlorenceTokenizer.json";
+        }
+        
+        public struct OverflowingTokenizer: ITokenizerConfig
+        {
+            public static uint ExpectedMaxInputLength => 384;
+
+            public static uint ExpectedMaxBatches => 2;
+            
+            public static string TokenizerJsonPath => "OverflowingTokenizer.json";
         }
     }
 }
